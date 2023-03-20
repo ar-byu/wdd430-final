7e92c064f6fcd65a9718d863ac3f7cf674e41934
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-20 19:46:27 -0400

    Started server file

---
+++ disneyAnimationApp/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
