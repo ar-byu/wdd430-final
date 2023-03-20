7983b409697bf9b4148380f949a2df93a8a54ca4
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-20 14:11:19 -0400

    Added routing

--- disneyAnimationApp/src/app/app-routing.module.ts
+++ disneyAnimationApp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ErasComponent } from './eras/eras.component';
+import { SpinoffsComponent } from './spinoffs/spinoffs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/movies', pathMatch: 'full'},
+  {path: 'movies', component: MoviesComponent},
+  {path: 'spinoffs', component: SpinoffsComponent},
+  {path: 'characters', component: CharactersComponent},
+  {path: 'eras', component: ErasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
