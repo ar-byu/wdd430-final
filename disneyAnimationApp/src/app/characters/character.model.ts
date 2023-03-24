8d3da0d5639be7cd80649689505e9059c0eab57a
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-23 20:26:59 -0400

    Added character-detail component, updated model

--- disneyAnimationApp/src/app/characters/character.model.ts
+++ disneyAnimationApp/src/app/characters/character.model.ts
@@ -1,13 +1,15 @@
 export class Character {
     public id: number;
     public name: string;
+    public movie: string;
     public role: string;
     public description: string;
     public trivia: string;
 
-    constructor(id: number, name: string, role: string, description: string, trivia: string) {
+    constructor(id: number, name: string, movie: string, role: string, description: string, trivia: string) {
         this.id = id;
         this.name = name;
+        this.movie = movie;
         this.role = role;
         this.description = description;
         this.trivia = trivia;
