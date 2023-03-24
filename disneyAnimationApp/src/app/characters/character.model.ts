2e602a9b794345669bfdb15e48d1838409e8ef8f
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-24 10:40:11 -0400

    Added image property to characters

--- disneyAnimationApp/src/app/characters/character.model.ts
+++ disneyAnimationApp/src/app/characters/character.model.ts
@@ -1,14 +1,16 @@
 export class Character {
     public id: number;
     public name: string;
+    public image: string;
     public movie: string;
     public role: string;
     public description: string;
     public trivia: string;
 
-    constructor(id: number, name: string, movie: string, role: string, description: string, trivia: string) {
+    constructor(id: number, name: string, image: string, movie: string, role: string, description: string, trivia: string) {
         this.id = id;
         this.name = name;
+        this.image = image;
         this.movie = movie;
         this.role = role;
         this.description = description;
