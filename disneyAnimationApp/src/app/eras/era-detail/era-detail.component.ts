0fd9f6d4af25cc32b7337dbecc5f52bb5839ec70
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-24 10:52:21 -0400

    Added era detail routing

--- disneyAnimationApp/src/app/eras/era-detail/era-detail.component.ts
+++ disneyAnimationApp/src/app/eras/era-detail/era-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Era } from '../eras.model';
+import { ErasService } from '../eras.service';
 
 @Component({
   selector: 'app-era-detail',
   templateUrl: './era-detail.component.html',
   styleUrls: ['./era-detail.component.css']
 })
-export class EraDetailComponent {
+export class EraDetailComponent implements OnInit {
+  era: Era;
+  id: number;
+
+  constructor(private erasService: ErasService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+        this.era = this.erasService.getOneEra(this.id);
+      }
+    )
+  }
 
 }
--- disneyAnimationApp/src/app/eras/era-item/era-item.component.ts
+++ disneyAnimationApp/src/app/eras/era-item/era-item.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Era } from '../eras.model';
+import { ErasService } from '../eras.service';
 
 @Component({
   selector: 'app-era-item',
   templateUrl: './era-item.component.html',
   styleUrls: ['./era-item.component.css']
 })
-export class EraItemComponent {
+export class EraItemComponent implements OnInit {
   @Input() era: Era;
+  @Input() index: number;
+
+  constructor(private erasService: ErasService) {}
+
+  ngOnInit() {}
 }
