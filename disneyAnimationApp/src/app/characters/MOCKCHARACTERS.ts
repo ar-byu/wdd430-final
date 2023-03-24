4062cd115897a40858a17a1ceed97c8bae81738c
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-23 20:34:11 -0400

    Updated mock characters, character detail

--- disneyAnimationApp/src/app/characters/MOCKCHARACTERS.ts
+++ disneyAnimationApp/src/app/characters/MOCKCHARACTERS.ts
@@ -2,6 +2,7 @@ export const MOCKCHARACTERS: any[] = [
     {
         "id": 1,
         "name": "Thumper",
+        "movie": "Bambi",
         "role": "Protagonist",
         "description": "Bambi's close rabbit friend. He is loud and mischevious, but tries to advise his friends when he can.",
         "trivia": "Thumper was originally meant to have a smaller role, but was given a larger one when some of the writers heard his voice actor."
@@ -9,6 +10,7 @@ export const MOCKCHARACTERS: any[] = [
       {
         "id": 2,
         "name": "Dumbo",
+        "movie": "Dumbo",
         "role": "Protagonist",
         "description": "Officially named Jumbo Junior, Dumbo is a baby elephant with very large ears. He has the personality of a small child, being playful and innocent.",
         "trivia": "Dumbo is the only titular character in any Disney film to have no speaking lines."
@@ -16,6 +18,7 @@ export const MOCKCHARACTERS: any[] = [
       {
         "id": 3,
         "name": "Flower",
+        "movie": "Bambi",
         "role": "Protagonist",
         "description": "A coy and soft-spoken skunk and another good friend of Bambi. He is friendly and is shown as having a consistently positive outlook on life.",
         "trivia": "Whether Flower's name is his real, given name or not is unknown."
@@ -23,6 +26,7 @@ export const MOCKCHARACTERS: any[] = [
       {
         "id": 4,
         "name": "Beast",
+        "movie": "Beauty and the Beast",
         "role": "Protagonist",
         "description": "Originally called Prince Adam, the Beast is a young prince who was turned into a beast by an enchantress after being rude to her. He seems to be an antagonist at first, but grows to be kinder over the course of the movie.",
         "trivia": "The Beast's design is a mix of several animals, including a buffalo, lion, bear, gorilla, and boar."
@@ -30,6 +34,7 @@ export const MOCKCHARACTERS: any[] = [
       {
         "id": 5,
         "name": "Lilo",
+        "movie": "Lilo & Stitch",
         "role": "Protagonist",
         "description": "A five year old girl with a world of her own. She lives in Hawaii with her older sister, who took custody of her after their parents died. She's known for having behavioral issues, likely from the trauma of losing her parents.",
         "trivia": "In further appearances, Lilo seems to be the ambassador of Earth to what is known as the Galactic Federation."
