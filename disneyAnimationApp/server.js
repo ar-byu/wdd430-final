ce1e7f277e4714df63e3f9d19395ce62f70361ad
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-04-02 15:15:38 -0400

    Started adding HTTP and Express

--- disneyAnimationApp/server.js
+++ disneyAnimationApp/server.js
@@ -5,3 +5,9 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+
+let index = require('./server/routes/app');
+const characterRoutes = require('./server/routes/characters');
+const eraRoutes = require('./server/routes/eras');
+const movieRoutes = require('./server/routes/movies');
+const spinoffRoutes = require('./server/routes/spinoffs');
