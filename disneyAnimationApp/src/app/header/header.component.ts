09a2e90d507f17683a7c8fea41b8d4fca8f43c0f
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-04-05 12:30:03 -0400

    Added custom styling, active nav link styling

--- disneyAnimationApp/src/app/header/header.component.ts
+++ disneyAnimationApp/src/app/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  selectedLink: string = 'movies';
 
+  navClick(event, item) {
+    this.selectedLink = item;
+  }
 }
